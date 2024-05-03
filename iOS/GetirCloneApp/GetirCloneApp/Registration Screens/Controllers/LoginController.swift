47e514a07ce15b744f579ff88bbe4d69164f693e
Ayberk Bilgiç <lewidvailed@outlook.com>
2024-05-03 23:49:47 +0300

    nav bar appearance and login screen update

--- iOS/GetirCloneApp/GetirCloneApp/Registration Screens/Controllers/LoginController.swift
+++ iOS/GetirCloneApp/GetirCloneApp/Registration Screens/Controllers/LoginController.swift
@@ -2,7 +2,7 @@
 //  LoginController.swift
 //  GetirCloneApp
 //
-//  Created by Ayberk Bilgiç on 24.02.2024.
+//  Created by Ayberk Bilgiç on 3.05.2024.
 //
 
 import UIKit
@@ -10,19 +10,16 @@ import UIKit
 class LoginController: UIViewController {
 
     // MARK: - UI Components
-    private let headerView = AuthHeaderView(title: "Tatlımı Getir'e Üye Ol!")
-    private let usernameField = CustomTextField(fieldType: .username)
-    private let passwordField = CustomTextField(fieldType: .password)
-    private let emailField = CustomTextField(fieldType: .email)
-    private let firstChexbox = Checkbox(isChecked: false)
-    private let secondChexbox = Checkbox(isChecked: false)
-    private let registerButton = CustomButton(title: "Üye Ol")
+    private let headerViewLogin = AuthHeaderView(title: "Tatlımı Getir'e Giriş Yap!")
+    private let usernameFieldLogin = CustomTextField(fieldType: .username)
+    private let passwordFieldLogin = CustomTextField(fieldType: .password)
+    private let loginButton = CustomButton(title: "Giriş Yap")
     
-    private let termsTextView: UITextView = {
+    private let termsTextViewLogin: UITextView = {
         let textView = UITextView()
         
-        let attributedString = NSMutableAttributedString(string: "Kullanım Koşulları'nı okudum ve kabul ediyorum")
-        attributedString.addAttribute(.link, value: "terms://termsOfConditions", range: (attributedString.string as NSString).range(of: "Kullanım Koşulları"))
+        let attributedString = NSMutableAttributedString(string: "Kişisel verilerine dair Aydınlatma Metni için tıklayınız.")
+        attributedString.addAttribute(.link, value: "terms://termsOfConditions", range: (attributedString.string as NSString).range(of: "tıklayınız."))
         
         textView.linkTextAttributes = [.foregroundColor: UIColor(red: 92/255, green: 60/255, blue: 187/255, alpha: 1)]
         textView.attributedText = attributedString
@@ -35,21 +32,13 @@ class LoginController: UIViewController {
         return textView
     }()
     
-    private let policyTextView: UITextView = {
-        let textView = UITextView()
+    private let reminderButton: UIButton = {
+        let rmdButton = UIButton()
+        let color = UIColor(red: 92/255, green: 60/255, blue: 187/255, alpha: 1)
+        rmdButton.backgroundColor = .white
         
-        let attributedString = NSMutableAttributedString(string: "KVKK Aydınlatma Metni kapsamında verilerimin bana özel teklifler ve kampanyalar için kullanılmasına, tarafıma ticari elektronik ileti gönderilmesine izin veriyorum")
-        attributedString.addAttribute(.link, value: "privacy://privacyPolicy", range: (attributedString.string as NSString).range(of: "KVKK Aydınlatma Metni"))
-
-        textView.linkTextAttributes = [.foregroundColor: UIColor(red: 92/255, green: 60/255, blue: 187/255, alpha: 1)]
-        textView.attributedText = attributedString
-        textView.textColor = .label
-        textView.backgroundColor = .clear
-        textView.isEditable = false
-        textView.isSelectable = true
-        textView.isScrollEnabled = false
-        textView.delaysContentTouches = false
-        return textView
+        rmdButton.setTitleColor(color ,for: .normal)
+        return rmdButton
     }()
     
     // MARK: - Lifecycle
@@ -57,124 +46,52 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         self.setupUI()
         
-        self.termsTextView.delegate = self
-        self.policyTextView.delegate = self
+        self.makeOpaqueNavBar()
         
     }
     
     // MARK: - UI Setup
+
     private func setupUI() {
-        self.view.addSubview(headerView)
-        self.view.addSubview(emailField)
-        self.view.addSubview(usernameField)
-        self.view.addSubview(passwordField)
-        self.view.addSubview(firstChexbox)
-        self.view.addSubview(secondChexbox)
-        self.view.addSubview(termsTextView)
-        self.view.addSubview(policyTextView)
-        self.view.addSubview(registerButton)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        emailField.translatesAutoresizingMaskIntoConstraints = false
-        usernameField.translatesAutoresizingMaskIntoConstraints = false
-        passwordField.translatesAutoresizingMaskIntoConstraints = false
-        firstChexbox.translatesAutoresizingMaskIntoConstraints = false
-        secondChexbox.translatesAutoresizingMaskIntoConstraints = false
-        termsTextView.translatesAutoresizingMaskIntoConstraints = false
-        policyTextView.translatesAutoresizingMaskIntoConstraints = false
-        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(headerViewLogin)
+        self.view.addSubview(usernameFieldLogin)
+        self.view.addSubview(passwordFieldLogin)
+        self.view.addSubview(loginButton)
+        self.view.addSubview(termsTextViewLogin)
         
-        
-        
-        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
-        navigationController?.navigationBar.titleTextAttributes = textAttributes
+        headerViewLogin.translatesAutoresizingMaskIntoConstraints = false
+        usernameFieldLogin.translatesAutoresizingMaskIntoConstraints = false
+        passwordFieldLogin.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        termsTextViewLogin.translatesAutoresizingMaskIntoConstraints = false
         
         self.view.backgroundColor = .systemBackground
-        self.navigationItem.title = "Üye ol"
+        self.navigationItem.title = "Giriş Yap"
         
-        
-        self.navigationController?.navigationBar.backgroundColor = UIColor(red: 92/255, green: 60/255, blue: 187/255, alpha: 1)
-        self.navigationItem.largeTitleDisplayMode = .never
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.left")?.withTintColor(.white, renderingMode: .alwaysOriginal), style: .plain, target: self, action: #selector(backButtonTapped))
-        
-
         NSLayoutConstraint.activate([
-            self.headerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            self.headerView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            self.headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            self.headerView.heightAnchor.constraint(equalToConstant: 160),
+            self.headerViewLogin.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.headerViewLogin.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.headerViewLogin.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.headerViewLogin.heightAnchor.constraint(equalToConstant: 160),
             
-            self.usernameField.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 20),
-            self.usernameField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.usernameField.heightAnchor.constraint(equalToConstant: 55),
-            self.usernameField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            self.usernameFieldLogin.topAnchor.constraint(equalTo: headerViewLogin.bottomAnchor, constant: 20),
+            self.usernameFieldLogin.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.usernameFieldLogin.heightAnchor.constraint(equalToConstant: 55),
+            self.usernameFieldLogin.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
             
-            self.emailField.topAnchor.constraint(equalTo: usernameField.bottomAnchor, constant: 20),
-            self.emailField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.emailField.heightAnchor.constraint(equalToConstant: 55),
-            self.emailField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            self.passwordFieldLogin.topAnchor.constraint(equalTo: usernameFieldLogin.bottomAnchor, constant: 20),
+            self.passwordFieldLogin.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.passwordFieldLogin.heightAnchor.constraint(equalToConstant: 55),
+            self.passwordFieldLogin.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
             
-            self.passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 20),
-            self.passwordField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.passwordField.heightAnchor.constraint(equalToConstant: 55),
-            self.passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
-            
-            self.firstChexbox.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 20),
-            self.firstChexbox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 25),
-            
-            self.termsTextView.leadingAnchor.constraint(equalTo: firstChexbox.trailingAnchor),
-            self.termsTextView.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 20),
-            self.termsTextView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
-            
-            self.secondChexbox.topAnchor.constraint(equalTo: firstChexbox.bottomAnchor),
-            self.secondChexbox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 25),
-            
-            self.policyTextView.leadingAnchor.constraint(equalTo: secondChexbox.trailingAnchor),
-            self.policyTextView.topAnchor.constraint(equalTo: firstChexbox.bottomAnchor),
-            self.policyTextView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            self.termsTextViewLogin.topAnchor.constraint(equalTo: passwordFieldLogin.bottomAnchor, constant: 20),
+            self.termsTextViewLogin.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.termsTextViewLogin.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 25),
+            self.termsTextViewLogin.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
         
-            self.registerButton.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor),
-            self.registerButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            self.registerButton.heightAnchor.constraint(equalToConstant: 50),
-            self.registerButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+        
+        
         ])
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.insetsLayoutMarginsFromSafeArea = false
-
-    }
-    
-    @objc func backButtonTapped() {
-        print("button pressed")
-    }
-    
-
 }
-
-extension LoginController: UITextViewDelegate {
-     
-    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange) -> Bool {
-         
-        if URL.scheme == "terms" {
-            self.showWebViewController(with: "https://getir.com/yardim/kullanim-kosullari/")
-        } else if URL.scheme == "privacy" {
-            self.showWebViewController(with: "https://getir.com/yardim/kvkk/#")
-        }
-        return true
-    }
-    
-    func textViewDidChange(_ textView: UITextView) {
-        textView.delegate = nil
-        textView.selectedTextRange = nil
-        textView.delegate = self
-    }
-    
-    private func showWebViewController(with urlString: String) {
-        let vc = WebViewController(with: urlString)
-        let navigation = UINavigationController(rootViewController: vc)
-        self.present(navigation, animated: true, completion: nil)
-    }
-    
-}
-
---
+++ iOS/GetirCloneApp/GetirCloneApp/Registration Screens/extension/Extension.swift
@@ -0,0 +1,40 @@
+//
+//  Extension.swift
+//  GetirCloneApp
+//
+//  Created by Ayberk Bilgiç on 2.05.2024.
+//
+
+import Foundation
+
+import UIKit.UIViewController
+
+extension UIViewController {
+
+    func makeOpaqueNavBar() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor(red: 92/255, green: 60/255, blue: 187/255, alpha: 1)
+            appearance.shadowColor = .clear
+            appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+            navigationController?.navigationBar.standardAppearance = appearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        }
+    }
+
+
+    func makeTransclucentNavBar() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self else { return }
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            appearance.backgroundColor = .clear
+            appearance.shadowColor = .clear
+            navigationController?.navigationBar.standardAppearance = appearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+            navigationController?.isNavigationBarHidden = false
+        }
+    }
+}
