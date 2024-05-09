87ca59af3d9629b30520fa48200338154759e11c
Ayberk Bilgiç <lewidvailed@outlook.com>
2024-05-09 17:45:13 +0300

    collectionview update

--- iOS/GetirCloneApp/GetirCloneApp/Main Screens/Controllers/Home/Views/CollectionTableViewCell.swift
+++ iOS/GetirCloneApp/GetirCloneApp/Main Screens/Controllers/Home/Views/CollectionTableViewCell.swift
@@ -7,17 +7,74 @@
 
 import UIKit
 
+struct CollectionTableViewCellViewModel {
+    let viewModels: [TileCollectionViewCellViewModel]
+}
+
 class CollectionTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier = "CollectionTableViewCell"
+    
+    private var viewModels: [TileCollectionViewCellViewModel] = []
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(TileCollectionViewCell.self, forCellWithReuseIdentifier: TileCollectionViewCell.identifier)
+        collectionView.backgroundColor = .systemBackground
+        
+        return collectionView
+    }()
+    
+    // MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.backgroundColor = .systemBackground
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Layout
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+    func configure(with viewModel: CollectionTableViewCellViewModel) {
+        self.viewModels = viewModel.viewModels
+        collectionView.reloadData()
+    }
+    
+    
+}
 
+extension CollectionTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+     
+    // MARK: - CollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TileCollectionViewCell.identifier, for: indexPath) as? TileCollectionViewCell else {
+            fatalError()
+        }
+        cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = contentView.frame.size.width
+        return CGSize(width: width, height: width/1.5)
+    }
+    
 }
