2561783aefdc6b8c548db49a2dcc29f1d8763c8c
Ayberk Bilgiç <lewidvailed@outlook.com>
2024-06-27 22:54:31 +0300

    firebase authservice update

---
+++ iOS/GetirCloneApp/GetirCloneApp/Models/RegisterUserRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterUserRequest.swift
+//  GetirCloneApp
+//
+//  Created by Ayberk Bilgiç on 27.06.2024.
+//
+
+import Foundation
+
+struct RegisterUserRequest {
+    let email: String
+    let username: String
+    let password: String
+}
---
+++ iOS/GetirCloneApp/GetirCloneApp/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  GetirCloneApp
+//
+//  Created by Ayberk Bilgiç on 27.06.2024.
+//
+
+import Foundation
+
+struct User {
+    let username: String
+    let email: String
+    let userUID: String
+}
